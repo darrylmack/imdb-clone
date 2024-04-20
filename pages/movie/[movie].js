81f19dec8abf01edf115c02b405a9ad123140bcd
Darryl Mack <darryl@mackmedia.io>
2024-04-20 14:40:24 -0700

    have to close out

--- pages/movie/[movie].js
+++ pages/movie/[movie].js
@@ -4,17 +4,13 @@ import { useRouter } from 'next/router'
 import { ThumbUpIcon } from '@heroicons/react/outline'
 import YouTube from 'react-youtube'
 import Moment from 'react-moment'
+import { render } from 'react-dom'
 
 const Detail = ({ movie }) => {
   const router = useRouter()
   const [showTrailer, setShowTrailer] = useState(false)
-  const [trailers, setTrailers] = useState(false)
-
-  useEffect(() => {
-    if (videos?.results.length > 0) {
-      setTrailers(true)
-    }
-  }, [])
+  const [trailers, setTrailers] = useState([])
+  const [featurettes, setFeaturettes] = useState([])
 
   const baseURL = 'https://image.tmdb.org/t/p/original'
 
@@ -35,6 +31,57 @@ const Detail = ({ movie }) => {
     videos
   } = movie
 
+  useEffect(() => {
+    let trailerList = []
+    if (videos?.results.length > 0) {
+      videos.results.forEach((video) => {
+        trailerList.push(video)
+      })
+    }
+    setTrailers(trailerList)
+    getFeaturettes()
+  }, [])
+
+  const getFeaturettes = () => {
+    let featuretteList = []
+    if (videos?.results.length > 0) {
+      videos.results.forEach((video) => {
+        if (video.type === 'Featurette') {
+          featuretteList.push(video)
+        }
+      })
+    }
+    setFeaturettes(featuretteList)
+  }
+
+  const renderFeaturettes = () => {
+    const options = {
+      width: '100%',
+      height: '100%',
+      playerVars: {
+        autoplay: 1,
+        controls: 0
+      }
+    }
+
+    if (featurettes.length > 0) {
+      return featurettes.map((video) => {
+        console.log(video)
+        return (
+          <div className="max-w-screen mx-auto">
+            <div className="w-[100%] aspect-w-16 aspect-h-9 bg-black">
+              <YouTube
+                videoId={video.key}
+                opts={options}
+                iframeClassName={'object-cover min-w-full h-[100%]'}
+              />
+            </div>
+          </div>
+        )
+      })
+    }
+  }
+
   const getTrailer = () => {
     const options = {
       width: '100%',
@@ -45,13 +92,13 @@ const Detail = ({ movie }) => {
       }
     }
 
-    const officialTrailer = videos.results.find(
+    const officialTrailer = trailers.find(
       (video) => video.name === 'Official Trailer'
     )
 
     if (officialTrailer?.key && showTrailer) {
       return (
-        <div className="max-w-screen mx-auto">
+        <div className="bg-gray-900 select-none p-2  sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           <div className="w-[100%] aspect-w-16 aspect-h-9 bg-black">
             <YouTube
               videoId={officialTrailer.key}
@@ -65,8 +112,8 @@ const Detail = ({ movie }) => {
 
     if (!officialTrailer) {
       return (
-        <div className="max-w-screen mx-auto">
-          <div className="w-[100%] aspect-w-16 aspect-h-9 bg-black">
+        <div className="bg-gray-900 select-none p-2  sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <div className="w-[240px] aspect-w-16 aspect-h-9 bg-black">
             <YouTube
               videoId={videos.results[0].key}
               opts={options}
@@ -103,6 +150,8 @@ const Detail = ({ movie }) => {
           />
         )}
       </div>
+
+      <div>{featurettes && renderFeaturettes()}</div>
 
       <div className="p-4">
         <p className=" text-base pb-2">{overview}</p>
