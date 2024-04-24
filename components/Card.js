476c5c92fb29681bc04fab0e0f405902ea06595e
Darryl Mack <darryl@mackmedia.io>
2024-04-24 10:51:30 -0700

    Links on genres

--- components/Card.js
+++ components/Card.js
@@ -25,32 +25,34 @@ const Card = ({ result }) => {
     }
   }
 
- 
-
   const renderGenres = () => {
     return (
       <div>
-        {result.genre_ids.map((genreId, index, array) => {
-          // Find the genre in the genres array
-          const genre = genres.find(g => g.id === genreId);
-          return (
-            <Fragment key={genreId}>
-              <a href={`/genres/${genreId}`}>{genre ? genre.name : 'Unknown'}</a>
-              {index !== array.length - 1 && ', '}
-            </Fragment>
-          );
-        })}
+        {result.genre_ids
+          .map((genreId) => {
+            const genre = genres.find(g => g.id === genreId);
+            return genre ? (
+              <Fragment key={genreId}>
+                <a className='hover:text-white ' href={`/genres/${genreId}`}>{genre.name}</a>
+              </Fragment>
+            ) : null;
+          })
+          .filter(genre => genre !== null)
+          .slice(0, 3)
+          .reduce((prev, curr) => [prev, ', ', curr])}
       </div>
     );
   };
+  
   
 
 console.log('RESULT:', result)
 
 const vote_average = result.vote_average.toFixed(1)
   return (
-    <div className="mb-4  sm:mb-0 p-2 text-gray-300 cursor-pointer hover:text-white  active:text-red-400 ">
+    <div className="mb-4  sm:mb-0 p-2 text-gray-300   active:text-red-400 ">
       <Image
+      className='cursor-pointer'
         onClick={goToDetail}
         aspectratio={500 / 750}
         src={`${baseURL}${result.poster_path}`}
@@ -67,7 +69,7 @@ const vote_average = result.vote_average.toFixed(1)
           <div className='flex border-gray-500 border-2 rounded-md px-2 py-1'>IMDb:<p className=' pl-1 font-semibold'>{vote_average}</p></div>
           { true ?         <HeartIconSolid className="h-5 text-red-400 ml-4 mr-0" /> : <HeartIcon className="h-5 ml-3 mr-1"/>}
         </div>
-        <div className='flex'>{renderGenres()}</div>
+        <div className='flex '>{renderGenres()}</div>
 
       </div>
     </div>
