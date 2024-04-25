30c640bf84f4ca602a09c0458bcfabc681d16590
Darryl Mack <darryl@mackmedia.io>
2024-04-24 19:33:34 -0700

    Replaced Switch/Case with If statement. Added icons to indicate media type

--- components/Card.js
+++ components/Card.js
@@ -3,27 +3,34 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Moment from 'react-moment'
 import { useRouter } from 'next/router'
-import { ThumbUpIcon, HeartIcon } from '@heroicons/react/outline'
+import {  HeartIcon, FilmIcon } from '@heroicons/react/outline'
+
+
+
 import { HeartIcon as HeartIconSolid } from '@heroicons/react/solid'
 import { genres } from '../utils/genres'
+
+const TvIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="gray" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z" />
+</svg>
+
+  )
+}
 
 const Card = ({ result }) => {
   const router = useRouter()
   const baseURL = 'https://image.tmdb.org/t/p/original'
+  const { media_type } = result
+
   const goToDetail = () => {
-
-    const { media_type } = result
-
-    switch (media_type) {
-      case 'tv':
-        router.push(`/tv/${result.id}`)
-      case 'movie':
-        router.push(`/movie/${result.id}`)
-        break
-
-      default:
-        break
+    if(media_type === 'movie') {
+      router.push(`/movie/${result.id}`)
+    } else {
+      router.push(`/tv/${result.id}`)
     }
+
   }
 
   const renderGenres = () => {
@@ -47,7 +54,7 @@ const Card = ({ result }) => {
   
   
 
-console.log('RESULT:', result)
+
 
 const vote_average = result.vote_average.toFixed(1)
   return (
@@ -68,6 +75,7 @@ const vote_average = result.vote_average.toFixed(1)
 
         <div className="flex justify-between align-center font-light py-1">
           <div className='flex border-gray-500 border-2 rounded-md px-2 py-1'>IMDb:<p className=' pl-1 font-semibold'>{vote_average}</p></div>
+          <div>{media_type === 'movie' ? <FilmIcon className="h-5 ml-3 mr-1 text-gray-300"/>: <TvIcon className="h-5 ml-3 mr-1"/>}</div>
           { true ?         <HeartIconSolid className="h-5 text-red-400 ml-4 mr-0" /> : <HeartIcon className="h-5 ml-3 mr-1"/>}
         </div>
         <div className='flex '>{renderGenres()}</div>
